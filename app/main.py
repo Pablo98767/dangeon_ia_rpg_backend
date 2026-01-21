6a3c3303d94d9881311a47a7ec9f55821c719fd3
Pablo Felix <89041026+Pablo98767@users.noreply.github.com>
2026-01-21 09:37:27 -0300

    Uncomment storydungeon.com.br in allowed origins

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ def create_app() -> FastAPI:
             "http://localhost:3000",      # React dev server
             "http://127.0.0.1:3000",      # React dev server alternativo
             "https://dungeon-generator-frontend.onrender.com", # frontend em produção
-            #"https://storydungeon.com.br"
+            "https://storydungeon.com.br"
         ],
         allow_credentials=True,
         allow_methods=["*"],              # Permite todos os métodos (GET, POST, OPTIONS, etc)
